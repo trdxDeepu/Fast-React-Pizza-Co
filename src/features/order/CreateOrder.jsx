269bae2b2cfbffa490d0be27ab8c1373e2aa6e8e
trdxlucifer <singhdepu566@gmail.com>
2023-09-19 15:25:25 +0530

    using router can take form data

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,9 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
+import { Form, redirect } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -38,7 +43,7 @@ function CreateOrder() {
     <div>
       <h2>Ready to order? Let's go!</h2>
 
-      <form>
+      <Form method="POST" >
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -69,12 +74,33 @@ function CreateOrder() {
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
-        <div>
+        <div> 
+          <input type="hidden" name="cart" value={JSON.stringify(cart)}/>
           <button>Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
 }
+
+export async function action({request}){
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  console.log(data)
+
+  const order = {
+    ...data,
+    cart:JSON.parse(data.cart),
+    priority: data.priority === 'on'
+  }
+
+  const newOrder = await createOrder(order);
+  console.log(newOrder)
+  
+  return redirect(`/orders/${newOrder.id}`)
+ 
+}
+
+
 
 export default CreateOrder;
