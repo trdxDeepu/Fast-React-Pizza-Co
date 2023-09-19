73b98aaa8e201fe3d28943020a4f53532fff9bac
trdxlucifer <singhdepu566@gmail.com>
2023-09-19 10:44:31 +0530

    created search order to fetch the order and loader to loading the fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home";
-import Menu,{loader as menuLoader} from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order,{loader as orderLoader} from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
-import Error from './ui/Error'
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
-    errorElement:<Error/>,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
-        errorElement:<Error/>,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
@@ -34,6 +34,7 @@ const router = createBrowserRouter([
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader:orderLoader,
       },
     ],
   },
