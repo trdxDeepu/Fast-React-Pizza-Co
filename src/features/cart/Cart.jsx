2c73cc13230fa0f5f31a390bb077794c35b74846
trdxlucifer <singhdepu566@gmail.com>
2023-09-21 11:36:25 +0530

    clear cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,54 +1,39 @@
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCart } from './cartSlice';
+import EmptyCart from './EmptyCart';
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+
 
 function Cart() {
   // eslint-disable-next-line no-unused-vars
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
+  const dispatch = useDispatch();
 
-  const username = useSelector(state=>state.user.userName)
+  const username = useSelector((state) => state.user.userName);
+
+  if(!cart.length) return <EmptyCart/>
 
   return (
     <div className="px-3 py-3 font-robotoMono  ">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
       <h2 className="mt-7 text-xl font-semibold ">Your cart, {username}</h2>
-      <ul className='divide-y divide-stone-200 border-b mt-3'>
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
           <CartItem item={item} key={item.key} />
         ))}
       </ul>
-      <div className='mt-6 space-x-2 '>
+      <div className="mt-6 space-x-2 ">
         <Button to="/order/new" type="primary">
-          Order pizzas  
+          Order pizzas
         </Button>
 
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
